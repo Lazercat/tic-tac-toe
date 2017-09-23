88ea514eacb26d9e6c7121a9cbec3ebc54afba04
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-09-23 09:13:08 -0500

    Nice save Point!

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
   /* KNOWN ISSUES
   ****************
-   1).   Reset function doesn't reload page, it clears the board with innerHTML
-         It can't reset the turnNumber variable which is a problem.
-
-   2).   Reset function brings the initial 'a' values back from god knows where. they've
-         been removed from the initial index page.
-
-   3)    Scoring functionality doesn't exist yet.
-
-   4)    No way to inform that a character has won yet.
+   1) Reset function doesn't reload page, it clears the board with innerHTML
+      It can't reset the turnNumber variable which is a problem.
+   2) Scoring functionality doesn't exist yet.
+   3) No way to inform that a character has won yet.
   *********************/
 
   // wait for the DOM to finish loading
@@ -19,12 +14,12 @@
   /*  TARGET
     <div class="col-md-6 col-md-offset-3" id="board">
     <section id="r1" class="row">
-      <div id="tl1" class="col-md-4 box row1 col1 diag1">row1 col1 diag1</div>
-      <div id="tc2" class="col-md-4 box row1 col2"> row1 col2</div>
-      <div id="tr3" class="col-md-4 box row1 col3 diag2">row1 col3 diag2</div>
+      <div id="r1" class="col-md-4 box row1 col1 diag1">row1 col1 diag1</div>
+      <div id="r1" class="col-md-4 box row1 col2"> row1 col2</div>
+      <div id="r1" class="col-md-4 box row1 col3 diag2">row1 col3 diag2</div>
     </section>
     <section id="r2" class="row">
-      <div id="ml4" class="col-md-4 box row2 col1">row2 col1</div>
+      <div id="r2" class="col-md-4 box row2 col1">row2 col1</div>
       <div id="mc5" class="col-md-4 box row2 col2 diag1 diag2">row2 col2 diag1 diag2</div>
       <div id="mr6" class="col-md-4 box row2 col3">row2 col3</div>
     </section>
@@ -35,26 +30,65 @@
     </section>
    *******************************/
 
-
-
   /* ELEMENTS
   ********************/
-   var ecsi = '<img src="img/x.gif" alt="x" height="100%">';
-   var ohsi = '<img src="img/o.gif" alt="o" height="100%">';
-   var winner;
+   var ecsi = '<img id="x" src="img/x.gif" alt="x" height="100%">';
+   var ohsi = '<img id="o" src="img/o.gif" alt="o" height="100%">';
+   var winner = null;
+   var xwins = 0;
+   var ywins = 0;
+   var round = 1;
+
+
+   /* WINNER Announcement
+    ********************/
+   function winYell(win){
+    console.log(win + 'Is the Winner of Round ' + round);
+    round++;
+    console.log('now the round is ' + round);
+    return round;
+   }
+
+
+    /* WINNER TRACKER
+  ********************/
+  function winTrack(winner){
+
+    //IS A ROW ALL ONE KIND?
+    var rw1x = [...document.querySelectorAll('.row1 > img#x')];
+    var rw1o = [...document.querySelectorAll('.row1 > img#o')];
+    var rw2x = [...document.querySelectorAll('.row2 > img#x')];
+    var rw2o = [...document.querySelectorAll('.row2 > img#o')];
+    var rw3x = [...document.querySelectorAll('.row3 > img#x')];
+    var rw3o = [...document.querySelectorAll('.row3 > img#o')];
+
+    if (rw1x.length === 3){
+      winner = "Player X";
+      winYell(winner);
+    }
+
+    console.log('i did things today:' + rw1x);
+
+    //IS A COLUMN ALL ONE KIND?
+
+    //IS THERE A DIAGONAL?
+
+  }
+
 
   /* GLOBAL VARIABLES
   *********************/
-    var squares = [...document.querySelectorAll('.col-md-4')]; //es6
-    console.log(squares);
+    var squares = [...document.querySelectorAll('.col-md-4')];
+    console.log("yo squares: " + squares);
     var turnNumber=0;
     var playerUp = document.querySelector('.current-player');
-
 
   /* USER CLICKS IN GRID
   ************************/
     squares.forEach(function(square) {
     square.addEventListener('click', function(event) {
+
+
 
     //SQUARE IS AVAILABLE TO PLAY
     if(square.innerHTML!=ecsi && square.innerHTML!=ohsi){
@@ -68,7 +102,9 @@
        if(turnRemain===0){square.innerHTML=ecsi;playerUp.innerHTML='Player 1(O) Turn!';} //Put an x on it
        else if (turnRemain===1){square.innerHTML=ohsi;playerUp.innerHTML='Player 2(X) Turn!';} //put an o on it
 
-      return turnNumber;
+
+        winTrack(winner);
+        return turnNumber;
     }
 
     //SQUARE IS NOT AVAILABLE TO PLAY. ALERT USER.
@@ -83,35 +119,17 @@
 /* RESET GRID
 ********************/
  function resetGrid(){
-    var squares = [...document.querySelectorAll('.col-md-4')]; //es6
+    var squares = [...document.querySelectorAll('.col-md-4')];
     var x = document.querySelector('.current-player');
     x.innerHTML = 'Player 1(O) Turn!';
     var turnNumber=0;
      squares.forEach(function(squareReset){
-      squareReset.innerHTML = "a";
+      squareReset.innerHTML = " ";
       console.log("reset grid");
    }) //close forEach
  } //close resetGrid
 
 
-
-  /* WINNER TRACKER
-  ********************/
-  function winTrack(){
-
-    //IS A ROW ALL ONE KIND?
-    var row1 = [...document.getSelectorAll('.row1')];
-    console.log(row1);
-
-
-    //IS A COLUMN ALL ONE KIND?
-
-
-    //IS THERE A DIAGONAL?
-
-
-  }
-  winTrack();
 
 
 
